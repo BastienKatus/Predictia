339401f5a4f0d7d98e74ea0cda37e6544b3d7c80
Faycalth <faycalthamri38@gmail.com>
2024-01-11 11:29:36 +0100

    deleting next_games scripts

--- data/init-scripts/init_soccer_manager.sql
+++ data/init-scripts/init_soccer_manager.sql
@@ -136,10 +136,15 @@ alter sequence "next_games_seq" owner to admin;
 CREATE TABLE next_games (
     next_game_id INTEGER PRIMARY KEY NOT NULL,
     home_club_id INTEGER,
-    home_club_short_name VARCHAR(255),
+    home_club_short_name TEXT,
     away_club_id INTEGER,
-    away_club_short_name VARCHAR(255),
-    prediction FLOAT
+    away_club_short_name TEXT,
+    status TEXT,
+    game_date DATE,
+    modified_date_verification DATE,
+    prediction_win_home FLOAT,
+    prediction_win_away FLOAT,
+    prediction_draw FLOAT
 );
 
 CREATE TABLE player_valuations (
